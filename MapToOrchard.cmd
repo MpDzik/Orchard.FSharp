4a974269e452923f15bba92e2f43e24323d3a719
Marek Dzikiewicz <marek.dzikiewicz@proligence.pl>
2015-02-02 00:51:32 +0100

    Create Orchard.FSharp module

--- MapToOrchard.cmd
+++ MapToOrchard.cmd
@@ -4,6 +4,8 @@ if "%1" == "" goto usage
 
 mklink /J %1\src\Orchard.Web\Modules\Orchard.FSharp %~dp0src\Orchard.FSharp
 mklink /J %1\src\Orchard.Tests.Modules.FSharp %~dp0src\Orchard.Tests.Modules.FSharp
+mklink /J %1\lib\dynamitey %~dp0lib\dynamitey
+mklink /J %1\lib\fsharp-dynamic %~dp0lib\fsharp-dynamic
 goto done
 
 :usage
